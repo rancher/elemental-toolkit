afb6fb0bad2df835379866f3bd487fdb3f8f667d
Itxaka <igarcia@suse.com>
2021-05-20 13:18:38 +0200

    WIP some error output
    Signed-off-by: Itxaka <igarcia@suse.com>

--- tests/upgrades-images-unsigned/upgrade_test.go
+++ tests/upgrades-images-unsigned/upgrade_test.go
@@ -1,6 +1,7 @@
 package cos_test
 
 import (
+	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/rancher-sandbox/cOS/tests/sut"
@@ -21,11 +22,14 @@ var _ = Describe("cOS Upgrade tests - Images unsigned", func() {
 		When("images are not signed", func() {
 			It("upgrades to latest available (master) and reset", func() {
 				out, err := s.Command("cos-upgrade")
+				fmt.Fprintf(GinkgoWriter, "Output from cos-upgrade: %v\n", out)
+				fmt.Fprintf(GinkgoWriter, "Error from cos-upgrade: %v\n", err)
 				Expect(err).ToNot(HaveOccurred())
 				Expect(out).Should(ContainSubstring("Upgrade done, now you might want to reboot"))
 				Expect(out).Should(ContainSubstring("Booting from: active.img"))
 				By("rebooting")
 				s.Reboot()
+				fmt.Fprintf(GinkgoWriter, "Bootfrom is: %v\n", s.BootFrom())
 				Expect(s.BootFrom()).To(Equal(sut.Active))
 			})
 			It("upgrades on an unsigned upgrade channel", func() {
