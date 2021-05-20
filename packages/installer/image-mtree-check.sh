109f4b90eb81d0c0bb47430daf19ce2baadadc2a
Itxaka <itxakaserrano@gmail.com>
2021-05-20 23:32:55 +0200

    Update image-mtree-check.sh

--- packages/installer/image-mtree-check.sh
+++ packages/installer/image-mtree-check.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-exec >> /tmp/image-mtree-check.log
-exec 2>&1
+# This connects stdout+stderr to the log file but leaves fd3 connected to the console
+# To log to both use | tee /dev/fd/3 which sends the data to fd3 AND to tee fd1 which is connected to the log file
+# You can also use 1>&3 to send it to console only
+exec 3>&1 1>>/tmp/image-mtree-check.log 2>&1
+
 event="$1"
 
 if [ "$event" == "package.install" ]; then
@@ -21,8 +24,10 @@ if [ "$event" == "package.install" ]; then
 
   luet util unpack "$image" "$tmpdir"
   yq read "$tmpdir"/"$name"-"$category"-"$version".metadata.yaml mtree > "$mtree_output"
-  luet mtree -- check /tmp/upgrade "$mtree_output"
+  luet_result=$(luet mtree -- check /tmp/upgrade "$mtree_output" -f json)
   rm "$mtree_output"
   rm  -Rf "$tmpdir"
-  exit $?
+  echo "$luet_result" | tee /dev/fd/3
+else
+  echo "{}" | tee /dev/fd/3
 fi
