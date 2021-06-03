a243aa58bd63a65e861bd3bb8e6420c3ca548ce0
Itxaka <igarcia@suse.com>
2021-06-03 10:24:28 +0200

    Reenable more test related to signed images
    Now that verify is in for all types of upgrade, re enable some of the
    tests that we dropped before
    
    Signed-off-by: Itxaka <igarcia@suse.com>

--- tests/upgrades-images-unsigned/upgrade_test.go
+++ tests/upgrades-images-unsigned/upgrade_test.go
@@ -20,7 +20,7 @@ var _ = Describe("cOS Upgrade tests - Images unsigned", func() {
 	})
 	Context("After install", func() {
 		When("images are not signed", func() {
-			It("upgrades to latest available (master) and reset", func() {
+			It("upgrades to latest available (master) with --no-verify and reset", func() {
 				out, err := s.Command("source /etc/os-release && echo $VERSION")
 				Expect(err).ToNot(HaveOccurred())
 				Expect(out).ToNot(Equal(""))
