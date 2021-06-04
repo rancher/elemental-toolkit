8bdb1b88a7a80be01b2dbe6aa0e16b4a00513d76
Itxaka <igarcia@suse.com>
2021-06-04 15:15:53 +0200

    [mtree] rework the mtree check script
    As we now support unpack events, both paths (channel upgrades or single
    docker image) send the same events now, so we can hijack that to have a
    single path with fewer lines of code.
    
    This patch simplifies the script to have one singular path that both
    upgrade methods use and verify the mtree checksums. It also cleans up a
    bit the logging from the script to be more useful.
    
    Signed-off-by: Itxaka <igarcia@suse.com>

--- packages/installer/image-mtree-check.sh
+++ packages/installer/image-mtree-check.sh
@@ -6,39 +6,30 @@ exec 3>&1 1>>/tmp/image-mtree-check.log 2>&1
 
 event="$1"
 
-if [ "$event" == "package.install" ] || [ "$event" == "image.post.unpack" ]; then
-  payload="$2"
-
-  if [ "$event" == "package.install" ]; then
-    # Extract all data from the payload
-    file=$(echo "$payload" | jq -r .file)
-    version=$(jq -r .Package.version < "$file")
-    repo=$(jq -r .Repository.urls[0] < "$file")
-    name=$(jq -r .Package.name < "$file")
-    category=$(jq -r .Package.category < "$file")
-    download_version=$(echo "$version"|tr "+" "-")
-    image="$repo":"$name"-"$category"-"$download_version".metadata.yaml
-    mtree_output=/tmp/"$name"-"$category"-"$download_version".mtree
-    tmpdir=/tmp/"$name"-"$category"-"$download_version"-metadata
-    destination=/tmp/upgrade
-  else
-    # Use the image name as base for everything
-    fullimage=$(echo "$2" | jq -r .data | jq -r .Image )
-    imagetype=$(echo "$fullimage" |cut -d ":" -f 2)
-    # We should skip when unpacking the repository and tree images as they do not contain mtree values
-    if [[ $imagetype == *"repository.yaml"* ]] || [[ $imagetype == *"tree.tar"* ]] || [[ $imagetype == *"repository.meta.yaml.tar"* ]] || [[ $imagetype == *"compilertree.tar"* ]]; then
-      echo "{}" | tee /dev/fd/3
-      exit 0
-    fi
-    destination=$(echo "$2" | jq -r .data | jq -r .Dest)
-    image="$fullimage.metadata.yaml"
-    tmpdir=/tmp/"$fullimage"-metadata
-    mtree_output=/tmp/"$fullimage".mtree
+if [ "$event" == "image.post.unpack" ]; then
+  echo "[$(date)] Got event $1, processing."
+  # Use the image name as base for everything
+  fullimage=$(echo "$2" | jq -r .data | jq -r .Image )
+  imagetype=$(echo "$fullimage" |cut -d ":" -f 2)
+  # We should skip when unpacking the repository and tree images as they do not contain mtree values
+  if [[ $imagetype == *"repository.yaml"* ]] || [[ $imagetype == *"tree.tar"* ]] || [[ $imagetype == *"repository.meta.yaml.tar"* ]] || [[ $imagetype == *"compilertree.tar"* ]]; then
+    echo "[$(date)] Got $imagetype, skipping"
+    echo "{}" 1>&3
+    exit 0
   fi
+  echo "[$(date)] Got $imagetype, continue..."
+  destination=$(echo "$2" | jq -r .data | jq -r .Dest)
+  image="$fullimage.metadata.yaml"
+  tmpdir=/tmp/"$fullimage"-metadata
+  mtree_output=/tmp/"$fullimage".mtree
 
-  echo "Getting $image metadata"
+  echo "[$(date)] Getting $image metadata"
 
-  luet util unpack "$image" "$tmpdir"
+  # TMPDIR here is a workaround because we are calling luet from inside luet and there is aggressive cleaning after the
+  # luet command finishes (https://github.com/mudler/luet/blob/master/cmd/root.go#L111) which will cleanup ALL luet tmp
+  # dirs, so we could be removing our recently unpacked image during install by using this
+  # This just moves the luet tmp dir to /tmp/
+  TMPDIR=/tmp luet util unpack "$image" "$tmpdir" >> /tmp/luet.log
 
   metadata=$(find "$tmpdir" -name "*.metadata.yaml")
 
@@ -48,20 +39,24 @@ if [ "$event" == "package.install" ] || [ "$event" == "image.post.unpack" ]; the
   rm  -Rf "$tmpdir"
   if [[ $luet_result == "" ]]; then
     # empty output means no errors
+    echo "[$(date)] Finished all checks with no errors"
     jq --arg key0 "state" --arg value0 "All checks succeeded" \
        --arg key1 "data"  --arg value1 "" \
        --arg key2 "error" --arg value2 "" \
        '. | .[$key0]=$value0 | .[$key1]=$value1 | .[$key2]=$value2' \
-    <<<'{}' | tee /dev/fd/3
+    <<<'{}' 1>&3
   else
+    echo "[$(date)] Finished all checks with errors"
     echo "$luet_result" > /tmp/luet_mtree_failures.log
     error_message="Error while checking, see /tmp/luet_mtree_failures.log for the full failures log"
     jq --arg key0 "state" --arg value0 "Checks failed" \
        --arg key1 "data"  --arg value1 "" \
        --arg key2 "error" --arg value2 "$error_message" \
        '. | .[$key0]=$value0 | .[$key1]=$value1 | .[$key2]=$value2' \
-    <<<'{}' | tee /dev/fd/3
+    <<<'{}' 1>&3
+    exit 1
   fi
 else
-  echo "{}" | tee /dev/fd/3
+  echo "{}" 1>&3
+  exit 0
 fi
