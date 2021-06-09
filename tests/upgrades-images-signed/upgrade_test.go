c5e0f18fb29f23d023a5466436d06675bac0bb48
Itxaka <igarcia@suse.com>
2021-06-09 14:37:01 +0200

    Use the new luet-mtree event for upgrade checks
    We were using a bash script to call luet-mtree on upgrade to check for
    valid mtree checksums.
    
    luet-mtree 0.0.6 now supports working with events from luet itself,
    which will do the same as the bash script and extract the image metadata
    to check the mtree checksums.
    
    This patch removes the old bash script and uses the luet-mtree plugin
    directly to check the checksums, it also bumps luet-mtree to 0.0.6
    
    Signed-off-by: Itxaka <igarcia@suse.com>

--- tests/upgrades-images-signed/upgrade_test.go
+++ tests/upgrades-images-signed/upgrade_test.go
@@ -20,6 +20,7 @@ var _ = Describe("cOS Upgrade tests - Images signed", func() {
 			s.GatherLog("/tmp/image-mtree-check.log")
 			s.GatherLog("/tmp/luet_mtree_failures.log")
 			s.GatherLog("/tmp/luet_mtree.log")
+			s.GatherLog("/tmp/luet.log")
 		}
 		s.Reset()
 	})
