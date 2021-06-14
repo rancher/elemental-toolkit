eb675d08559d111a405eaef2a2a2534a0ffdd495
Ettore Di Giacinto <edigiacinto@suse.com>
2021-06-14 10:02:39 +0200

    Allow to upgrade from local directories
    - Bump installer
    - Add test for upgrades from directory
    - Add missing labels for autobump
    
    Co-authored-by: Darren Shepherd <darren@rancher.com>
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- tests/upgrades-local/upgrade_test.go
+++ tests/upgrades-local/upgrade_test.go
@@ -2,54 +2,37 @@ package cos_test
 
 import (
 	"fmt"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/rancher-sandbox/cOS/tests/sut"
 )
 
-var _ = Describe("cOS Upgrade tests - Images unsigned", func() {
+var _ = Describe("cOS Upgrade tests - local upgrades", func() {
 	var s *sut.SUT
-	var isVagrant bool
-
-	BeforeSuite(func() {
-		isVagrant = sut.IsVagrantTest()
-		if isVagrant {
-			sut.SnapshotVagrant()
-		}
-	})
-
-	AfterSuite(func() {
-		if isVagrant {
-			sut.SnapshotVagrantDelete()
-		}
-	})
 
 	BeforeEach(func() {
 		s = sut.NewSUT()
 		s.EventuallyConnects(360)
 	})
 
-	AfterEach(func() {
-		if CurrentGinkgoTestDescription().Failed == false {
-			if isVagrant {
-				sut.ResetWithVagrant()
-			} else {
-				s.Reset()
-			}
-
-		}
-	})
-	Context("After install", func() {
-		When("images are not signed", func() {
-			It("upgrades to latest available (master) with --no-verify", func() {
+	Context("After install can upgrade and reset", func() {
+		When("specifying a directory to upgrade from", func() {
+			It("upgrades from the specified path", func() {
 				out, err := s.Command("source /etc/os-release && echo $VERSION")
 				Expect(err).ToNot(HaveOccurred())
 				Expect(out).ToNot(Equal(""))
 
 				version := out
-				out, err = s.Command("cos-upgrade --no-verify --docker-image quay.io/costoolkit/releases-opensuse:cos-system-0.5.1")
-				if err != nil {
+
+				out, err = s.Command("mkdir /run/update && luet util unpack quay.io/costoolkit/releases-opensuse:cos-system-0.5.1 /run/update")
+				if err != nil{
+					fmt.Fprintf(GinkgoWriter, "Error from luet util unpack: %v\n", err)
+				}
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).ToNot(Equal(""))
+
+				out, err = s.Command("cos-upgrade --no-verify --directory /run/update")
+				if err != nil{
 					fmt.Fprintf(GinkgoWriter, "Error from cos-upgrade: %v\n", err)
 				}
 				Expect(err).ToNot(HaveOccurred())
