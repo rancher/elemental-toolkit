332662b8c685110dd1053097728e510aa3f54198
David Cassany <dcassany@suse.com>
2021-06-15 13:00:12 +0200

    Add raw_disk Makefile target
    Signed-off-by: David Cassany <dcassany@suse.com>

--- images/img-builder.sh
+++ images/img-builder.sh
@@ -6,25 +6,6 @@ rm -rf ./*.part disk.raw grub_efi.cfg recovery root .luet.yaml
 
 set -e
 
-truncate -s $((20*1024*1024)) efi.part
-
-# Create the EFI partition FAT16 and include the EFI image and a basic grub.cfg
-mkfs.fat -F16 -n EFI efi.part
-mmd -i efi.part ::EFI
-mmd -i efi.part ::EFI/BOOT
-mcopy -i efi.part /usr/share/grub2/x86_64-efi/grub.efi ::EFI/BOOT/bootx64.efi
-
-# Creat the EFI grub.cfg pointing the configs in COS_RECOVERY volume
-cat << 'EOF' > grub_efi.cfg
-search.fs_label COS_RECOVERY root
-set root=($root)
-set prefix=($root)/grub2
-configfile ($root)/grub2/grub.cfg
-EOF
-
-# Copy grub.cfg in EFI partition
-mcopy -i efi.part grub_efi.cfg ::EFI/BOOT/grub.cfg
-
 # Create a luet config for local repositories
 cat << EOF > .luet.yaml
 repositories:
@@ -49,6 +30,25 @@ luet install --system-target root/cOS -y recovery/cos-img
 truncate -s $((2048*1024*1024)) rootfs.part
 mkfs.ext2 -L COS_RECOVERY -d root rootfs.part
 
+# Create the EFI partition FAT16 and include the EFI image and a basic grub.cfg
+truncate -s $((20*1024*1024)) efi.part
+
+mkfs.fat -F16 -n EFI efi.part
+mmd -i efi.part ::EFI
+mmd -i efi.part ::EFI/BOOT
+mcopy -i efi.part recovery/usr/share/grub2/x86_64-efi/grub.efi ::EFI/BOOT/bootx64.efi
+
+# Creat the EFI grub.cfg pointing the configs in COS_RECOVERY volume
+cat << 'EOF' > grub_efi.cfg
+search.fs_label COS_RECOVERY root
+set root=($root)
+set prefix=($root)/grub2
+configfile ($root)/grub2/grub.cfg
+EOF
+
+# Copy grub.cfg in EFI partition
+mcopy -i efi.part grub_efi.cfg ::EFI/BOOT/grub.cfg
+
 # Create a 64MB filesystem for COS_OEM volume
 truncate -s $((64*1024*1024)) oem.part
 mkfs.ext2 -L COS_OEM oem.part
@@ -61,10 +61,10 @@ truncate -s $((3*1024*1024)) disk.raw
     cat rootfs.part
 } >> disk.raw
 
-# Add an extra MB at the end of the disk for the gpt headers, in fact 34 sectors would be enough, adding some more does not hurt.
+# Add an extra MB at the end of the disk for the gpt headers, in fact 34 sectors would be enough, but adding some more does not hurt.
 truncate -s "+$((1024*1024))" disk.raw
 
-# Create the partition table in disk.raw
+# Create the partition table in disk.raw (assumes sectors of 512 bytes)
 sgdisk -n 1:2048:+2M -c 1:legacy -t 1:EF02 disk.raw
 sgdisk -n 2:0:+20M -c 2:UEFI -t 2:EF00 disk.raw
 sgdisk -n 3:0:+64M -c 3:oem -t 3:8300 disk.raw
