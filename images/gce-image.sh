e7b044ff63fb546823389f516291419e034527c1
Itxaka <igarcia@suse.com>
2021-07-13 17:49:09 +0200

    images: Build them at the same time
    Build all images in the same job as they can be built alongside and we
    dont spend time downloading the raw image each time.
    
    Plus that allows us to set the proper cos-version to all of them and
    upload them all at the same time
    
    This apthc also adds the version to the artifact and the sha built
    
    Signed-off-by: Itxaka <igarcia@suse.com>

--- images/gce-image.sh
+++ images/gce-image.sh
@@ -2,20 +2,15 @@
 
 # Transform a raw image disk to gce compatible
 RAWIMAGE="$1"
-RAW_DISK="disk.raw"
-COS_VERSION=$(yq r packages/cos/collection.yaml 'packages.[0].version')
-
 
 GB=$((1024*1024*1024))
 size=$(qemu-img info -f raw --output json "$RAWIMAGE" | gawk 'match($0, /"virtual-size": ([0-9]+),/, val) {print val[1]}')
 # shellcheck disable=SC2004
 ROUNDED_SIZE=$(echo "$size/$GB+1"|bc)
-if [[ "$RAWIMAGE" != "$RAW_DISK" ]]; then
-  echo "Renaming raw image to $RAW_DISK"
-  mv "$RAWIMAGE" "$RAW_DISK"
-fi
-echo "Resizing raw image from $size to $ROUNDED_SIZE"
-qemu-img resize -f raw "$RAW_DISK" "$ROUNDED_SIZE"G
-echo "Compressing raw image"
-tar -c -z --format=oldgnu -f cOS-Vanilla-"$COS_VERSION".tar.gz $RAW_DISK
+echo "Resizing raw image from \"$size\"MB to \"$ROUNDED_SIZE\"GB"
+qemu-img resize -f raw "$RAWIMAGE" "$ROUNDED_SIZE"G
+echo "Compressing raw image $RAWIMAGE to $RAWIMAGE.tar.gz"
+tar -c -z --format=oldgnu -f "$RAWIMAGE".tar.gz $RAWIMAGE
+echo "Restoring size to original raw image"
+qemu-img resize -f raw "$RAWIMAGE" --shrink "$size"
 echo "Done"
