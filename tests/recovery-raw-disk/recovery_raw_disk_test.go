ea13110c2c68ac163299fdacd6ecac44d6e5a3d0
Itxaka <igarcia@suse.com>
2021-08-20 10:30:27 +0200

    ci: Add test for deploying from raw image (#514)
    Recent events have shown that we do not test cos-deploy from a raw image
    as well as we should.
    
    This patch adds a simple test and preparation of a vm based of the raw
    image so we can test the the default cos-deploy stanza works as expected
    and lays the base so we can expand it with more tests in the future.
    
    Unfortunately as this test is fully modifying the disk image we can only
    test one thing per time, once we modify the disk the pristine state that
    we started with is gone, so we should probably create one work per test
    that we want.
    
    This also means that we can drop the normal raw-image-test that only
    checks if we can boot from the raw image properly as this new test already
    checks that part.
    
    Signed-off-by: Itxaka <igarcia@suse.com>

---
+++ tests/recovery-raw-disk/recovery_raw_disk_test.go
@@ -0,0 +1,33 @@
+package cos_test
+
+import (
+	"github.com/rancher-sandbox/cOS/tests/sut"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("cOS Recovery deploy tests", func() {
+	var s *sut.SUT
+
+	BeforeEach(func() {
+		s = sut.NewSUT()
+		s.EventuallyConnects()
+	})
+
+	Context("after running recovery from the raw_disk image", func() {
+		It("uses cos-deploy to install", func() {
+			ExpectWithOffset(1, s.BootFrom()).To(Equal(sut.Recovery))
+
+			out, err := s.Command("cos-deploy")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(out).Should(ContainSubstring("Upgrade done, now you might want to reboot"))
+
+			err = s.ChangeBoot(sut.Active)
+			Expect(err).ToNot(HaveOccurred())
+
+			s.Reboot()
+			ExpectWithOffset(1, s.BootFrom()).To(Equal(sut.Active))
+		})
+	})
+})
