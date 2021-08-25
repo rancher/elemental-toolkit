d962914255a365dbe42c25970fbec8a4388a3192
Itxaka <igarcia@suse.com>
2021-08-25 15:34:31 +0200

    ci: remove empty lines in the workflows
    Remove all the unnecessary empty lines that we generate as part of the
    pipelines script so its easier to comb thougth the lines
    
    Signed-off-by: Itxaka <igarcia@suse.com>

--- .github/pipelines.sh
+++ .github/pipelines.sh
@@ -3,4 +3,5 @@ set -ex
 
 for i in $(ls config); do 
     gomplate --left-delim "{{{" --right-delim "}}}" -V --datasource config=config/$i --file build.yaml.gomplate > workflows/build-$i
+    sed -i '/^[[:space:]]*$/d' workflows/build-$i
 done
