7b8c61e6fa81efef59bddc219c3198b94f4d76d1
Itxaka <igarcia@suse.com>
2021-09-08 21:53:37 +0200

    fix get_luet script
    Signed-off-by: Itxaka <igarcia@suse.com>

--- scripts/get_luet.sh
+++ scripts/get_luet.sh
@@ -13,8 +13,11 @@ LUET_DATABASE_PATH=${LUET_DATABASE_PATH:-/var/luet/db}
 LUET_DATABASE_ENGINE=${LUET_DATABASE_ENGINE:-boltdb}
 LUET_CONFIG_PROTECT=${LUET_CONFIG_PROTECT:-1}
 LUET_PACKAGE="${LUET_PACKAGE:-toolchain/luet}"
-LUET_ARCH="${LUET_ARCH:-amd64}"
+LUET_ARCH="${LUET_ARCH:-x86_64}"
 LUET_INSTALL_FROM_COS_REPO="${LUET_INSTALL_FROM_COS_REPO:-true}"
+if [[ "$LUET_ARCH" == "x86_64" ]]; then
+  LUET_ARCH="amd64" # stupid golang and their names for arches ¬_¬
+fi
 
 curl -L https://github.com/mudler/luet/releases/download/${LUET_VERSION}/luet-${LUET_VERSION}-linux-${LUET_ARCH} --output luet
 chmod +x luet
@@ -23,20 +26,6 @@ mkdir -p /etc/luet/repos.conf.d || true
 mkdir -p $LUET_DATABASE_PATH || true
 mkdir -p /var/tmp/luet || true
 
-cat > /etc/luet/luet.yaml <<EOF
-  general:
-    debug: false
-    enable_emoji: false
-  system:
-    rootfs: ${LUET_ROOTFS}
-    database_path: "${LUET_DATABASE_PATH}"
-    database_engine: "${LUET_DATABASE_ENGINE}"
-    tmpdir_base: "/var/tmp/luet"
-  general:
-     debug: false
-     spinner_charset: 9
-EOF
-
 if [[ "$LUET_INSTALL_FROM_COS_REPO" == "true" ]]; then
   if [[ "$LUET_ARCH" != "amd64" ]]; then
     REPO_URL="quay.io/costoolkit/releases-green-$LUET_ARCH"
@@ -44,17 +33,28 @@ if [[ "$LUET_INSTALL_FROM_COS_REPO" == "true" ]]; then
     REPO_URL="quay.io/costoolkit/releases-green"
   fi
 
-  cat >> /etc/luet/luet.yaml <<EOF
-  repositories:
-  - name: "cos"
-    description: "cOS official"
-    type: "docker"
-    enable: true
-    cached: true
-    priority: 1
-    verify: false
-    urls:
-    - ${REPO_URL}
+cat > /etc/luet/luet.yaml <<EOF
+general:
+  debug: false
+  enable_emoji: false
+system:
+  rootfs: ${LUET_ROOTFS}
+  database_path: "${LUET_DATABASE_PATH}"
+  database_engine: "${LUET_DATABASE_ENGINE}"
+  tmpdir_base: "/var/tmp/luet"
+general:
+   debug: false
+   spinner_charset: 9
+repositories:
+- name: "cos"
+  description: "cOS official"
+  type: "docker"
+  enable: true
+  cached: true
+  priority: 1
+  verify: false
+  urls:
+  - ${REPO_URL}
 EOF
   ./luet install -y $LUET_PACKAGE
   rm -rf luet
