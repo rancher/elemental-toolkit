58c36e96cce0e0e5748538d65d34ab66620a419a
Itxaka <igarcia@suse.com>
2021-09-10 16:00:53 +0200

    ci: support more than one arch per config
    Instead of having separated configs, lets be more intelligent and define
    the values per arch in the same config files
    
    Signed-off-by: Itxaka <igarcia@suse.com>

--- .github/pipelines.sh
+++ .github/pipelines.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -ex
 
-for i in $(ls config); do 
-    gomplate --left-delim "{{{" --right-delim "}}}" -V --datasource config=config/$i --file build.yaml.gomplate > workflows/build-$i
-    sed -i '/^[[:space:]]*$/d' workflows/build-$i
+for i in $(ls config); do
+    filename=$(basename -s .yaml $i)
+    for arch in $(yq r config/"$i" -p "p" 'arches.[*]' | cut -d "." -f 2); do
+      yq r config/"$i" "arches[$arch]" | gomplate --left-delim "{{{" --right-delim "}}}" -V --datasource config="stdin:/?type=application/yaml" --file build.yaml.gomplate --out workflows/build-"$filename"-"$arch".yaml
+      sed -i '/^[[:space:]]*$/d' workflows/build-"$filename"-"$arch".yaml
+    done
 done
