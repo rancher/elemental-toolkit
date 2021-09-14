035d0c91ea7866bae8b6a45bef43c01671a33deb
Ettore Di Giacinto <mudler@users.noreply.github.com>
2021-09-14 10:11:01 +0200

    ci: Tag toolchain images (#641)
    Fixes #640
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- .github/pipelines.sh
+++ .github/pipelines.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -ex
+YQ="${YQ:-yq}"
 
 for i in $(ls config); do
     filename=$(basename -s .yaml $i)
-    for arch in $(yq r config/"$i" -p "p" 'arches.[*]' | cut -d "." -f 2); do
-      yq r config/"$i" "arches[$arch]" | gomplate --left-delim "{{{" --right-delim "}}}" -V --datasource config="stdin:/?type=application/yaml" --file build.yaml.gomplate --out workflows/build-"$filename"-"$arch".yaml
+    for arch in $($YQ r config/"$i" -p "p" 'arches.[*]' | cut -d "." -f 2); do
+      $YQ r config/"$i" "arches[$arch]" | gomplate --left-delim "{{{" --right-delim "}}}" -V --datasource config="stdin:/?type=application/yaml" --file build.yaml.gomplate --out workflows/build-"$filename"-"$arch".yaml
       sed -i '/^[[:space:]]*$/d' workflows/build-"$filename"-"$arch".yaml
     done
 done
