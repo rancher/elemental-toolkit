9a5f1f9906dd09f934071f78ae941bb83a1e1d34
Itxaka <igarcia@suse.com>
2021-09-27 15:00:04 +0200

    ci: rework ci to split workflows by flavor+arch (#723)
    This patch introduces a new level on the config files for the workflows
    that refers to the flavor being tested.
    
    the templates are mostly unchanged as we already dealt with the
    different flavors correctly, the only thing tht needed a bit of change
    was the config file and those changes are mostly minimal thank to using
    anchors.
    
    Basically with this we split all the workflows by flavor+arch and allows
    us to have more fine tuning of each job. With also the addition of
    future switches (i.e. run_tests, release_github, etc...) we can have
    much more control per job instead of having all the options for all
    flavors in the same blob. As shown on this change, is now pretty clean
    the differences between the green and blue job for example and what is
    run on one vs the other.
    
    As a side effect, if one job fails we no longer have ot retriggers the
    full workflow with all the flavors, as that never made any sense to have
    them together, but insteaed trigger each individually
    
    Signed-off-by: Itxaka <igarcia@suse.com>

--- .github/pipelines.sh
+++ .github/pipelines.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-set -ex
+set -e
 YQ="${YQ:-yq}"
 
 for i in $(ls config); do
     filename=$(basename -s .yaml $i)
-    for arch in $($YQ r config/"$i" -p "p" 'arches.[*]' | cut -d "." -f 2); do
-      $YQ r config/"$i" "arches[$arch]" | gomplate --left-delim "{{{" --right-delim "}}}" -V --datasource config="stdin:/?type=application/yaml" --file build.yaml.gomplate --out workflows/build-"$filename"-"$arch".yaml
-      sed -i '/^[[:space:]]*$/d' workflows/build-"$filename"-"$arch".yaml
+    for flavor in $(${YQ} r config/"$i" -p "p" 'flavors.[*]' | cut -d "." -f 2); do
+      for arch in $(${YQ} r config/"$i" -p "p" "flavors.$flavor.arches.[*]" | cut -d "." -f 4); do
+        ${YQ} r -X config/"$i" "flavors.$flavor.arches.$arch" | gomplate --left-delim "{{{" --right-delim "}}}" -V --datasource config="stdin:/?type=application/yaml" --file build.yaml.gomplate --out workflows/build-"$filename"-"$flavor"-"$arch".yaml
+        sed -i '/^[[:space:]]*$/d' workflows/build-"$filename"-"$flavor"-"$arch".yaml
+      done
     done
 done
