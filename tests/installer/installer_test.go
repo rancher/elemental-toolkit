49855038a79e2321349c0e0d7312e3bf2ebea7d9
Itxaka <igarcia@suse.com>
2021-11-10 21:09:23 +0100

    Setup installer testing (#848)
    * ci: Add installer tests
    
     - Add make target for creating a vm from the iso
     - Add some generic tests to check if we booted from cd
     - Add placeholders for future tests for installer
    
    Signed-off-by: Itxaka <igarcia@suse.com>
    
    * ci: enable installer tests on ci
    
    Signed-off-by: Itxaka <igarcia@suse.com>
    
    * ci: Expand BootFrom to report LiveCD booting
    
    Signed-off-by: Itxaka <igarcia@suse.com>
    
    * prepare testing for bios+efi
    
    Signed-off-by: Itxaka <igarcia@suse.com>
    
    * ci: enable proper path for testing installer
    
    Signed-off-by: Itxaka <igarcia@suse.com>
    
    * ci: separate test-installer into bios+efi
    
    Now we have 2 different paths to test both boot methods.
    Both paths have distinct setups as they differ a bit in the boot steps
    
    Signed-off-by: Itxaka <igarcia@suse.com>
    
    * ci: enable both variants for installer test
    
    Both iso and efi can now run
    
    Signed-off-by: Itxaka <igarcia@suse.com>
    
    * ci: Store logs and serial for failed installer tests
    
    Signed-off-by: Itxaka <igarcia@suse.com>
    
    * ci: change order of cd ejecting from efi installer test
    
    Signed-off-by: Itxaka <igarcia@suse.com>
    
    * ci: increase timeout for installer tests
    
    Signed-off-by: Itxaka <igarcia@suse.com>

---
+++ tests/installer/installer_test.go
@@ -0,0 +1,125 @@
+package cos_test
+
+import (
+	"github.com/rancher-sandbox/cOS/tests/sut"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("cOS Installer tests", func() {
+	var s *sut.SUT
+
+	BeforeEach(func() {
+		s = sut.NewSUT()
+		s.EventuallyConnects()
+	})
+
+
+	Context("Using bios", func() {
+		BeforeEach(func() {
+			s.EmptyDisk("/dev/sda")
+			By("Reboot to make sure we boot from CD")
+			s.Reboot()
+			// Assert we are booting from CD before running the tests
+			By("Making sure we booted from CD")
+			ExpectWithOffset(1, s.BootFrom()).To(Equal(sut.LiveCD))
+			out, err := s.Command("grep /dev/sr /etc/mtab")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(out).To(ContainSubstring("iso9660"))
+			out, err = s.Command("df -h /")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(out).To(ContainSubstring("LiveOS_rootfs"))
+		})
+		AfterEach(func() {
+			if CurrentGinkgoTestDescription().Failed {
+				s.GatherAllLogs()
+			}
+		})
+		Context("install source tests", func() {
+			It("from iso", func() {
+				By("Running the cos-installer")
+				out, err := s.Command("cos-installer /dev/sda")
+				Expect(err).To(BeNil())
+				Expect(out).To(ContainSubstring("Copying cOS.."))
+				Expect(out).To(ContainSubstring("Installing GRUB.."))
+				Expect(out).To(ContainSubstring("Preparing recovery.."))
+				Expect(out).To(ContainSubstring("Preparing passive boot.."))
+				Expect(out).To(ContainSubstring("Formatting drives.."))
+				// Reboot so we boot into the just installed cos
+				s.Reboot()
+				By("Checking we booted from the installed cOS")
+				ExpectWithOffset(1, s.BootFrom()).To(Equal(sut.Active))
+			})
+			PIt("from url", func() {})
+		})
+		PContext("partition layout tests", func() {
+			It("with partition layout", func() {})
+		})
+		PContext("efi/gpt tests", func() {
+			It("forces gpt", func() {})
+			It("forces efi", func() {})
+		})
+		PContext("config file tests", func() {
+			It("uses a proper config file", func() {})
+			It("uses a wrong config file", func() {})
+		})
+	})
+	Context("Using efi", func() {
+		// EFI variant tests is not able to set the boot order and there is no plan to let the user do so according to virtualbox
+		// So we need to manually eject/insert the cd to force the boot order. CD inserted -> boot from cd, CD empty -> boot from disk
+		BeforeEach(func() {
+			// Assert we are booting from CD before running the tests
+			By("Making sure we booted from CD")
+			ExpectWithOffset(1, s.BootFrom()).To(Equal(sut.LiveCD))
+			out, err := s.Command("grep /dev/sr /etc/mtab")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(out).To(ContainSubstring("iso9660"))
+			out, err = s.Command("df -h /")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(out).To(ContainSubstring("LiveOS_rootfs"))
+		})
+		AfterEach(func() {
+			if CurrentGinkgoTestDescription().Failed {
+				s.GatherAllLogs()
+			}
+			// Store COS iso path
+			s.SetCOSCDLocation()
+			// Set Cos CD in the drive
+			s.RestoreCOSCD()
+			s.EmptyDisk("/dev/sda")
+			By("Reboot to make sure we boot from CD")
+			s.Reboot()
+		})
+		Context("install source tests", func() {
+			It("from iso", func() {
+				By("Running the cos-installer")
+				out, err := s.Command("cos-installer /dev/sda")
+				Expect(err).To(BeNil())
+				Expect(out).To(ContainSubstring("Copying cOS.."))
+				Expect(out).To(ContainSubstring("Installing GRUB.."))
+				Expect(out).To(ContainSubstring("Preparing recovery.."))
+				Expect(out).To(ContainSubstring("Preparing passive boot.."))
+				Expect(out).To(ContainSubstring("Formatting drives.."))
+				// Remove iso so we boot directly from the disk
+				s.EjectCOSCD()
+				// Reboot so we boot into the just installed cos
+				s.Reboot(360)
+				By("Checking we booted from the installed cOS")
+				ExpectWithOffset(1, s.BootFrom()).To(Equal(sut.Active))
+			})
+			PIt("from url", func() {})
+		})
+		PContext("partition layout tests", func() {
+			It("with partition layout", func() {})
+		})
+		PContext("efi/gpt tests", func() {
+			It("forces gpt", func() {})
+			It("forces efi", func() {})
+		})
+		PContext("config file tests", func() {
+			It("uses a proper config file", func() {})
+			It("uses a wrong config file", func() {})
+		})
+	})
+})
