74f92dfc746c97e7d69222e206f55391fc24ff5f
Itxaka <igarcia@suse.com>
2021-12-02 14:32:20 +0100

    Add tests for version and some utils for cmd testing
    Signed-off-by: Itxaka <igarcia@suse.com>

---
+++ cmd/version_test.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2021 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	. "github.com/onsi/gomega"
+	"github.com/rancher-sandbox/elemental-cli/internal/version"
+	"testing"
+)
+
+func TestVersion(t *testing.T) {
+	RegisterTestingT(t)
+	_, output, err := executeCommandC(rootCmd, "version")
+	Expect(err).To(BeNil())
+	v := version.Get().Version
+	Expect(output).To(ContainSubstring(v))
+}
+
+func TestVersionLong(t *testing.T) {
+	RegisterTestingT(t)
+	_, output, err := executeCommandC(rootCmd, "version", "--long")
+	Expect(err).To(BeNil())
+	v := version.Get().Version
+	Expect(output).To(ContainSubstring(v))
+	Expect(output).To(ContainSubstring("Version"))
+	Expect(output).To(ContainSubstring("GitCommit"))
+	Expect(output).To(ContainSubstring("GoVersion"))
+}
