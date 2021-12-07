76d26faaed68dd176292218735324f8badf38c77
Ettore Di Giacinto <mudler@users.noreply.github.com>
2021-12-07 20:56:28 +0100

    ci: add cleanup images plugin (#934)
    * ci: add cleanup images plugin
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * ci: update workflows
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * ci: fixup plugin installation
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * ci: cleanup cache after package is built or pushed
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * fixup cos-setup constraint
    
    This breaks validation as ver-1 is lower than ver
    
    See: https://github.com/rancher-sandbox/cOS-toolkit/pull/923
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- .github/build.sh
+++ .github/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo -E make PACKAGES="$1" build
+
+docker images --filter="reference=$REPO_CACHE" --format='{{.Repository}}:{{.Tag}}' | xargs -r docker rmi --force
+docker image prune --force
