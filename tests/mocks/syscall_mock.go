e6001913913b7b38e6306024e6767684f3011927
Itxaka <igarcia@suse.com>
2021-12-14 14:49:06 +0100

    Test failures on mount/unmount chroot
    Signed-off-by: Itxaka <igarcia@suse.com>

--- tests/mocks/syscall_mock.go
+++ tests/mocks/syscall_mock.go
@@ -1,11 +1,17 @@
 package mocks
 
+import "errors"
+
 type FakeSyscall struct {
 	chrootHistory []string // Track calls to chroot
+	ErrorOnChroot bool
 }
 
 func (f *FakeSyscall) Chroot(path string) error {
 	f.chrootHistory = append(f.chrootHistory, path)
+	if f.ErrorOnChroot {
+		return errors.New("chroot error")
+	}
 	return nil
 }
 
