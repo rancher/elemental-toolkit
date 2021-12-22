2833d6e57adb3ce52c09812202ba91ee077b6a99
Itxaka <igarcia@suse.com>
2021-12-22 11:11:30 +0100

    Add resigner and workflow to resign
    This works by getting the artifacts from the repo and checking their
    signatures. If those fail, it will try to resign the artifacts
    automatically and push those signatures.
    
    Signed-off-by: Itxaka <igarcia@suse.com>

---
+++ .github/resign.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mudler/luet/pkg/api/client"
+	"github.com/mudler/luet/pkg/api/core/types"
+	"github.com/mudler/luet/pkg/installer"
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+func getRepositoryPackages(repo string, ctx *types.Context) (searchResult client.SearchResult) {
+	tmpdir, err := ioutil.TempDir(os.TempDir(), "ci")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(tmpdir)
+	referenceID := os.Getenv("REFERENCEID")
+	if referenceID == "" {
+		referenceID = "repository.yaml"
+	}
+	d := installer.NewSystemRepository(types.LuetRepository{
+		Name:        "cOS",
+		Type:        "docker",
+		Cached:      true,
+		Urls:        []string{repo},
+		ReferenceID: referenceID,
+	})
+	ctx.Config.GetSystem().Rootfs = "/"
+	ctx.Config.GetSystem().TmpDirBase = tmpdir
+	re, err := d.Sync(ctx, false)
+	if err != nil {
+		panic(err)
+	} else {
+		for _, p := range re.GetTree().GetDatabase().World() {
+			searchResult.Packages = append(searchResult.Packages, client.Package{
+				Name:     p.GetName(),
+				Category: p.GetCategory(),
+				Version:  p.GetVersion(),
+			})
+		}
+		return
+	}
+}
+
+func main() {
+	// We want to be cool and keep the same format as luet, so we create the context here to pass around and use the logging functions
+	ctx := types.NewContext()
+	finalRepo := os.Getenv("FINAL_REPO")
+	if finalRepo == "" {
+		ctx.Error("A container repository must be specified with FINAL_REPO")
+		os.Exit(1)
+	}
+	cosignRepo := os.Getenv("COSIGN_REPOSITORY")
+	if cosignRepo == "" {
+		ctx.Error("A signature repository must be specified with COSIGN_REPOSITORY")
+		os.Exit(1)
+	}
+	packages := getRepositoryPackages(finalRepo, ctx)
+	for _, val := range packages.Packages {
+		imageTag := fmt.Sprintf("%s:%s", finalRepo, val.ImageTag())
+		checkAndSign(imageTag, ctx)
+	}
+	return
+}
+
+func checkAndSign(tag string, ctx *types.Context) {
+	var fulcioFlag string
+
+	ctx.Info("Checking artifact", tag)
+	tmpDir, _ := os.MkdirTemp("", "sign-*")
+	defer os.RemoveAll(tmpDir)
+
+	_ = os.Setenv("TUF_ROOT", tmpDir)         // TUF_DIR per run, we dont want to access the same files as another process
+	_ = os.Setenv("COSIGN_EXPERIMENTAL", "1") // Set keyless verify/sign
+
+	fulcioURL := os.Getenv("FULCIO_URL") // Allow to set a fulcio url
+	if fulcioURL != "" {
+		fulcioFlag = fmt.Sprintf("--fulcio-url=%s", fulcioURL)
+	}
+
+	_, err := exec.Command("cosign", "verify", tag).CombinedOutput()
+	if err != nil {
+		ctx.Warning("Artifact", tag, "has no signature, signing it")
+		out, err := exec.Command("cosign", fulcioFlag, "sign", tag).CombinedOutput()
+		if err != nil {
+			ctx.Error("Error signing", tag, ":", string(out))
+		} else {
+			ctx.Success("Artifact", tag, "signed")
+		}
+	} else {
+		ctx.Success("Artifact", tag, "has signature")
+	}
+}
