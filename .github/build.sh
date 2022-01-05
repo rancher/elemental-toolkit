a5802ae917f334b474309b387cc08528ec55c2e8
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-01-05 17:14:35 +0100

    Don't attempt to replace recovery when running in recovery mode on squashfs (#1035)
    * Do not attempt to rewrite recovery
    
    Fixes #1034
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * tests: split deploy-images test
    
    Otherwise, the second test will run the code inside the recovery, as the
    first test replaces the recovery in the VM too.
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * ci: cleanup all images after artifact build
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Downgrade elemental-cli fips
    
    Seems it fails to build
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * ci: Try to run on self-hosted
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * tests: tests-raw-disk only runs on macOS runners
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * ci: use local runners for master and release pipelines
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- .github/build.sh
+++ .github/build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 sudo -E make PACKAGES="$1" build
-
-docker images --filter="reference=$REPO_CACHE" --format='{{.Repository}}:{{.Tag}}' | xargs -r docker rmi --force
-docker image prune --force
