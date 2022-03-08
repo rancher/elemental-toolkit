e02a759ebbbaac18471e681fd810147282e60ccd
David Cassany <dcassany@suse.com>
2022-03-08 15:59:56 +0100

    Drop cos.sh script and cos-rebrand
    This commit makes `cos-deploy` to be a wrapper of `elemental reset` and
    drops `cos.sh` and `cos-rebrand` scripts.
    
    Signed-off-by: David Cassany <dcassany@suse.com>

--- packages/installer/README.md
+++ packages/installer/README.md
@@ -1,8 +1,7 @@
 This package provides the following utilities:
 
-- `cos-installer` ( wrapper of `cos --install` )
-- `cos-deploy` ( wrapper of `elemental install` )
+- `cos-installer` ( wrapper of `elemental install` )
+- `cos-deploy` ( wrapper of `elemental reset` )
 - `cos-reset` ( wrapper of `elemental reset` )
-- `cos-rebrand` ( wrapper of `cos --rebrand` )
 - `cos-upgrade` ( wrapper of `elemental upgrade` )
-- `suc-upgrade` ( wrapper of `cos --upgrade` )
+- `suc-upgrade` ( wrapper of `elemental upgrade` )
--- tests/deploys-images/deploy_test.go
+++ tests/deploys-images/deploy_test.go
@@ -2,6 +2,7 @@ package cos_test
 
 import (
 	"fmt"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -21,38 +22,19 @@ var _ = Describe("cOS Deploy tests", func() {
 		if CurrentGinkgoTestDescription().Failed {
 			s.GatherAllLogs()
 		}
-		if CurrentGinkgoTestDescription().Failed == false {
-			s.Reset()
-		}
-	})
-
-	Context("From active", func() {
-		When("deploying again", func() {
-			It("deploys only if --force flag is provided", func() {
-				By("deploying without --force")
-				out, err := s.Command(fmt.Sprintf("cos-deploy --docker-image %s:cos-system-%s", s.GreenRepo, s.TestVersion))
-				Expect(out).Should(ContainSubstring("There is already an active deployment"))
-				Expect(err).To(HaveOccurred())
-				By("deploying with --force")
-				out, err = s.Command(fmt.Sprintf("cos-deploy --force --docker-image %s:cos-system-%s", s.GreenRepo, s.TestVersion))
-				Expect(out).Should(ContainSubstring("Forcing overwrite"))
-				Expect(out).Should(ContainSubstring("now you might want to reboot"))
-				Expect(err).NotTo(HaveOccurred())
-			})
-		})
 	})
 
 	Context("From recovery", func() {
 		When("deploying again", func() {
-			It("deploys only if --force flag is provided", func() {
+			It("deploys/resets the system", func() {
 				err := s.ChangeBootOnce(sut.Recovery)
 				Expect(err).ToNot(HaveOccurred())
 				s.Reboot()
 				ExpectWithOffset(1, s.BootFrom()).To(Equal(sut.Recovery))
-				By("deploying with --force")
-				out, err := s.Command(fmt.Sprintf("cos-deploy --force --docker-image %s:cos-system-%s", s.GreenRepo, s.TestVersion))
-				Expect(out).Should(ContainSubstring("now you might want to reboot"))
+				_, err = s.Command(fmt.Sprintf("cos-deploy --docker-image %s:cos-system-%s", s.GreenRepo, s.TestVersion))
 				Expect(err).NotTo(HaveOccurred())
+				s.Reboot()
+				ExpectWithOffset(1, s.BootFrom()).To(Equal(sut.Active))
 			})
 		})
 	})
