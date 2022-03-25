35f9d20cd50942a5250090af3a0af283e0b5ba8c
Ettore Di Giacinto <edigiacinto@suse.com>
2022-03-25 09:55:52 +0100

    ci: Split up installer tests
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- tests/installer/tests_suite_test.go
+++ tests/installer/tests_suite_test.go
@@ -5,9 +5,17 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/rancher-sandbox/cOS/tests/sut"
 )
 
 func TestTests(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "cOS Installer test Suite")
+}
+
+func CheckPartitionValues(diskLayout sut.DiskLayout, entry sut.PartitionEntry) {
+	part, err := diskLayout.GetPartition(entry.Label)
+	Expect(err).To(BeNil())
+	Expect((part.Size / 1024) / 1024).To(Equal(entry.Size))
+	Expect(part.FsType).To(Equal(entry.FsType))
 }
