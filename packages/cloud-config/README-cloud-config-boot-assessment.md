b5d9767d3f820a3f0253075ff77235a569536ae8
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-05-05 18:51:36 +0200

    Automatic Boot assessment (#1276)
    * Add automatic boot assessment
    
    Fixes: #1146
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Fixup grub package build
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Bump cOS
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Respect next entry in boot assessment
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Rename GRUB variables
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Update docs
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Add cloud-init config to enable boot assessment on every boot
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Reword test
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * No need of sudo here
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

---
+++ packages/cloud-config/README-cloud-config-boot-assessment.md
@@ -0,0 +1,43 @@
+In order for a derivative to enable boot assessment, it is only required to have the cloud-config file installed.
+
+The boot assessment process works in the following way:
+
+- After an upgrade, sets a GRUB env sentinel variable indicating that we did run an upgrade
+- At the first boot, if we an upgrade was attempted, we set another sentinel variable, which indicates a booting attempt
+  - If we boot fine, sentinels are removed
+  - If we get back again at the GRUB menu, a failure must have occurred and we select the fallback entry, creating also
+    sentinels files and a specific cmdline option indicating we failed booting after an upgrade
+
+The grub sentinel files are in the COS_STATE partition, and get installed automatically after reset, install and upgrade:
+
+- `/boot_assessment` - contains the GRUB env sentinel variables
+- `/grub_boot_assessment` - contains the GRUB logic for booting into fallback
+
+Note: The extra GRUB logic is installed and sourced into `/grubcustom` inside `COS_STATE`.
+
+When a boot failure is detected and the fallback is automatically selected, it will be created a `/run/cos/upgrade_failure` sentinel file during the boot process, which is accessible under the `boot` cloud-init stage.
+
+To enable boot assessment besides upgrades, `enable_boot_assessment_always` can be set to `yes` in the grub environment ( for example in `/oem/grubenv` ).
+
+## Manually enabling boot assessment
+
+To manually trigger the boot assessment for the next boot, you can run the following in a active/passive booted system:
+
+```
+sudo mount -o rw,remount /run/initramfs/cos-state
+grub2-editenv /run/initramfs/cos-state/boot_assessment set enable_boot_assessment=yes
+sudo mount -o ro,remount /run/initramfs/cos-state
+```
+
+## Maintenance
+
+If the active partition fails, the boot assessment process will bring you back to the fallback partition. This is also the case if the dracut shell is displayed while dropping in emergency mode.
+
+If you are planning to do manual intervention and want to hook up with that console, you can disable the boot assessment process by running in the fallback partition:
+
+```
+sudo mount -o rw,remount /run/initramfs/cos-state
+grub2-editenv /run/initramfs/cos-state/boot_assessment set enable_boot_assessment=
+grub2-editenv /run/initramfs/cos-state/boot_assessment set boot_assessment_tentative=
+sudo mount -o ro,remount /run/initramfs/cos-state
+```
--- tests/fallback/fallback_test.go
+++ tests/fallback/fallback_test.go
@@ -1,6 +1,9 @@
 package cos_test
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/rancher-sandbox/cOS/tests/sut"
 
 	. "github.com/onsi/ginkgo"
@@ -22,6 +25,133 @@ var _ = Describe("cOS booting fallback tests", func() {
 			s.Reset()
 		}
 	})
+
+	bootAssessmentInstalled := func() {
+		// Auto assessment was installed
+		out, _ := s.Command("sudo cat /run/initramfs/cos-state/grubcustom")
+		Expect(out).To(ContainSubstring("bootfile_loc"))
+
+		out, _ = s.Command("sudo cat /run/initramfs/cos-state/grub_boot_assessment")
+		Expect(out).To(ContainSubstring("boot_assessment_blk"))
+
+		cmdline, _ := s.Command("sudo cat /proc/cmdline")
+		Expect(cmdline).To(ContainSubstring("rd.emergency=reboot rd.shell=0 panic=5"))
+	}
+
+	Context("image is corrupted", func() {
+		breakPaths := []string{"usr/lib/systemd", "bin/sh", "bin/bash", "usr/bin/bash", "usr/bin/sh"}
+		It("boots in fallback when rootfs is damaged, triggered by missing files", func() {
+			currentVersion := s.GetOSRelease("VERSION")
+
+			// Auto assessment was installed
+			bootAssessmentInstalled()
+
+			out, err := s.Command(fmt.Sprintf("elemental upgrade --no-verify --docker-image %s:cos-system-%s", s.GreenRepo, s.TestVersion))
+			Expect(err).ToNot(HaveOccurred(), out)
+			Expect(out).Should(ContainSubstring("Upgrade completed"))
+			Expect(out).Should(ContainSubstring("Upgrading active partition"))
+
+			out, _ = s.Command("sudo cat /run/initramfs/cos-state/boot_assessment")
+			Expect(out).To(ContainSubstring("enable_boot_assessment=yes"))
+
+			// Break the upgrade
+			out, _ = s.Command("sudo mount -o rw,remount /run/initramfs/cos-state")
+			fmt.Println(out)
+
+			out, _ = s.Command("sudo mkdir -p /tmp/mnt/STATE")
+			fmt.Println(out)
+
+			s.Command("sudo mount /run/initramfs/cos-state/cOS/active.img /tmp/mnt/STATE")
+
+			for _, d := range breakPaths {
+				out, _ = s.Command("sudo rm -rfv /tmp/mnt/STATE/" + d)
+			}
+
+			out, _ = s.Command("sudo ls -liah /tmp/mnt/STATE/")
+			fmt.Println(out)
+
+			out, _ = s.Command("sudo umount /tmp/mnt/STATE")
+			s.Command("sudo sync")
+
+			s.Reboot(700)
+
+			v := s.GetOSRelease("VERSION")
+			Expect(v).To(Equal(currentVersion))
+
+			cmdline, _ := s.Command("sudo cat /proc/cmdline")
+			Expect(cmdline).To(And(ContainSubstring("passive.img"), ContainSubstring("upgrade_failure")), cmdline)
+
+			Eventually(func() string {
+				out, _ := s.Command("sudo ls -liah /run/cos")
+				return out
+			}, 5*time.Minute, 10*time.Second).Should(ContainSubstring("upgrade_failure"))
+		})
+
+		It("without upgrades boots in fallback when rootfs is damaged, triggered by missing files", func() {
+			// Note, this double checks also that when we do a reset the boot assessment is re-installed
+			//  elemental reset wipes disks, so the boot-assessment code is re-installed via cloud-init, so we check
+			// also that
+			Expect(s.BootFrom()).To(Equal(sut.Active))
+
+			currentVersion := s.GetOSRelease("VERSION")
+
+			// Auto assessment was installed
+			bootAssessmentInstalled()
+
+			// No manual sentinel enabled
+			out, _ := s.Command("sudo cat /run/initramfs/cos-state/boot_assessment")
+			Expect(out).ToNot(ContainSubstring("enable_boot_assessment=yes"))
+
+			By("Reboot to recovery before reset")
+			err := s.ChangeBootOnce(sut.Passive)
+			Expect(err).ToNot(HaveOccurred())
+			s.Reboot()
+			Expect(s.BootFrom()).To(Equal(sut.Passive))
+
+			// Enable permanent boot assessment, break active.img
+			out, _ = s.Command("sudo mount -o rw,remount /run/initramfs/cos-state")
+			fmt.Println(out)
+
+			s.Command("sudo grub2-editenv /run/initramfs/cos-state/boot_assessment set enable_boot_assessment_always=yes")
+
+			// Break the upgrade
+			out, _ = s.Command("sudo mkdir -p /tmp/mnt/STATE")
+			fmt.Println(out)
+
+			s.Command("sudo mount /run/initramfs/cos-state/cOS/active.img /tmp/mnt/STATE")
+
+			for _, d := range breakPaths {
+				out, _ = s.Command("sudo rm -rfv /tmp/mnt/STATE/" + d)
+				fmt.Println(out)
+			}
+
+			out, _ = s.Command("sudo ls -liah /tmp/mnt/STATE/")
+			fmt.Println(out)
+
+			out, _ = s.Command("sudo umount /tmp/mnt/STATE")
+			s.Command("sudo sync")
+
+			s.Reboot(700)
+
+			v := s.GetOSRelease("VERSION")
+			Expect(v).To(Equal(currentVersion))
+
+			cmdline, _ := s.Command("sudo cat /proc/cmdline")
+			Expect(cmdline).To(And(ContainSubstring("passive.img"), ContainSubstring("upgrade_failure")), cmdline)
+
+			Eventually(func() string {
+				out, _ := s.Command("sudo ls -liah /run/cos")
+				return out
+			}, 5*time.Minute, 10*time.Second).Should(ContainSubstring("upgrade_failure"))
+
+			// Disable boot assessment
+			out, _ = s.Command("sudo mount -o rw,remount /run/initramfs/cos-state")
+			fmt.Println(out)
+
+			s.Command("sudo grub2-editenv /run/initramfs/cos-state/boot_assessment set enable_boot_assessment_always=")
+		})
+	})
+
 	Context("GRUB cannot mount image", func() {
 		When("COS_ACTIVE image was corrupted", func() {
 			It("fallbacks by booting into passive", func() {
@@ -35,6 +165,10 @@ var _ = Describe("cOS booting fallback tests", func() {
 				s.Reboot()
 
 				Expect(s.BootFrom()).To(Equal(sut.Passive))
+
+				// Here we did fallback from grub. boot assessment didn't kicked in here
+				cmdline, _ := s.Command("sudo cat /proc/cmdline")
+				Expect(cmdline).ToNot(And(ContainSubstring("upgrade_failure")), cmdline)
 			})
 		})
 		When("COS_ACTIVE and COS_PASSIVE images are corrupted", func() {
