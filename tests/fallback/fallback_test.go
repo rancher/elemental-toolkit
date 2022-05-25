363b8224ff55a9d9f0b7a860fbf8f78359831678
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-05-25 14:23:57 +0200

    Add fsck to dracut modules (#1352)
    * Add fsck to dracut modules
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Call fsck on each part found
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Handle tmp mount copy from different locations
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Enable fsck by default
    
    Derivatives can opt-out from it by specifying a custom bootargs.cfg
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Add test for fs integrity checks
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Bump grub config package
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Fine tune tests
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Incorporate feedback from review
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Properly install systemd-fsck
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>
    
    * Incorporate feedback from review
    
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- tests/fallback/fallback_test.go
+++ tests/fallback/fallback_test.go
@@ -151,6 +151,43 @@ var _ = Describe("cOS booting fallback tests", func() {
 		})
 	})
 
+	Context("COS_PERSISTENT partition is corrupted", func() {
+		It("boots in active when the persistent partition is damaged, and can be repaired with fsck", func() {
+
+			// Just to make sure we can match against the same output of blkid later on
+			// and that the starting condition is the one we expect
+			Eventually(func() string {
+				out, _ := s.Command("sudo blkid")
+				return out
+			}, 1*time.Minute, 10*time.Second).Should(ContainSubstring(`LABEL="COS_PERSISTENT"`))
+
+			persistent, err := s.Command("blkid -L COS_PERSISTENT")
+			Expect(err).ToNot(HaveOccurred())
+
+			// This breaks the partition so it can be fixed with fsck
+			_, err = s.Command("dd if=/dev/zero count=1 bs=4096 seek=0 of=" + persistent)
+			Expect(err).ToNot(HaveOccurred())
+
+			Eventually(func() string {
+				out, _ := s.Command("sudo blkid")
+				return out
+			}, 5*time.Minute, 10*time.Second).ShouldNot(ContainSubstring(`LABEL="COS_PERSISTENT"`))
+
+			s.Reboot()
+			s.EventuallyConnects(700)
+
+			Expect(s.BootFrom()).To(Equal(sut.Active))
+
+			// We should see traces of fsck in the journal.
+			// Note, this is a bit ugly because the only messages
+			// we have from systemd-fsck is just failed attempts to run.
+			// But this is enough for us to assess if it actually kicked in.
+			out, err := s.Command("sudo journalctl")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(out).To(ContainSubstring("systemd-fsck"))
+		})
+	})
+
 	Context("GRUB cannot mount image", func() {
 		When("COS_ACTIVE image was corrupted", func() {
 			It("fallbacks by booting into passive", func() {
