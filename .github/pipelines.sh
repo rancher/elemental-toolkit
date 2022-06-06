a7a2841fa8cf02a6b668ff7ab6cdb92db43b4ab6
Itxaka <igarcia@suse.com>
2022-06-06 17:51:05 +0200

    Move everything to yq4 (#1481)

--- .github/pipelines.sh
+++ .github/pipelines.sh
@@ -2,11 +2,18 @@
 set -e
 YQ="${YQ:-yq}"
 
+VERSION=$($YQ -V |rev|  cut -d " " -f 1 |rev| cut -d "." -f 1)
+if [[ "$VERSION" == "3" ]]; then
+    echo "yq version 3 detected, only version 4 is supported"
+    exit 1
+fi
+
 for i in $(ls config); do
     filename=$(basename -s .yaml $i)
-    for flavor in $(${YQ} r config/"$i" -p "p" 'flavors.[*]' | cut -d "." -f 2); do
-      for arch in $(${YQ} r config/"$i" -p "p" "flavors.$flavor.arches.[*]" | cut -d "." -f 4); do
-        ${YQ} r -X config/"$i" "flavors.$flavor.arches.$arch" | gomplate --left-delim "{{{" --right-delim "}}}" -V --datasource config="stdin:/?type=application/yaml" --file build.yaml.gomplate --out workflows/build-"$filename"-"$flavor"-"$arch".yaml
+    for flavor in $(${YQ} '.flavors|keys|join(" ")' config/"$i"); do
+      for arch in $(${YQ} ".flavors.$flavor.arches|keys|join(\" \")" config/"$i" ); do
+        # Use explode(.) so anchors are fully resolved before parsing
+        ${YQ} "explode(.)|.flavors.$flavor.arches.$arch" config/"$i" | gomplate --left-delim "{{{" --right-delim "}}}" -V --datasource config="stdin:/?type=application/yaml" --file build.yaml.gomplate --out workflows/build-"$filename"-"$flavor"-"$arch".yaml
         sed -i '/^[[:space:]]*$/d' workflows/build-"$filename"-"$flavor"-"$arch".yaml
       done
     done
