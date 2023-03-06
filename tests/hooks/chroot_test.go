a44c5f3fff8b78a50a6a58fcadf545daee3a231f
David Cassany <dcassany@suse.com>
2023-03-06 12:06:15 +0100

    Adding hooks test
    Signed-off-by: David Cassany <dcassany@suse.com>

--- tests/hooks/chroot_test.go
+++ tests/hooks/chroot_test.go
@@ -6,6 +6,10 @@ import (
 	sut "github.com/rancher-sandbox/ele-testhelpers/vm"
 )
 
+// TODO this it just for first round of tests, at some point we should use
+// an image base on a build for a tagged main commit.
+const upgradeImg = "ghcr.io/davidcassany/elemental-green:v0.10.1-29-g9ab23ba5"
+
 var _ = Describe("cOS Feature tests", func() {
 	var s *sut.SUT
 	BeforeEach(func() {
@@ -18,7 +22,7 @@ var _ = Describe("cOS Feature tests", func() {
 			err := s.SendFile("../assets/chroot_hooks.yaml", "/oem/chroot_hooks.yaml", "0770")
 			Expect(err).ToNot(HaveOccurred())
 
-			out, err := s.Command(s.ElementalCmd("upgrade"))
+			out, err := s.Command(s.ElementalCmd("upgrade", "--system.uri", upgradeImg))
 			Expect(err).ToNot(HaveOccurred())
 			Expect(out).Should(ContainSubstring("Upgrade completed"))
 			By("rebooting")
