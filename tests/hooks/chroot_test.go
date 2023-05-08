80204c3bf5d68bb7cb289145dd359fdeb2cc48a6
David Cassany <dcassany@suse.com>
2023-05-08 23:53:53 +0200

    Add fallback test
    Signed-off-by: David Cassany <dcassany@suse.com>

--- tests/hooks/chroot_test.go
+++ tests/hooks/chroot_test.go
@@ -10,10 +10,6 @@ import (
 	comm "github.com/rancher/elemental-toolkit/tests/common"
 )
 
-// TODO this it just for first round of tests, at some point we should use
-// an image base on a build for a tagged main commit.
-//const upgradeImg = "ghcr.io/davidcassany/elemental-green:v0.10.1-29-g9ab23ba5"
-
 var _ = Describe("Elemental Feature tests", func() {
 	var s *sut.SUT
 	BeforeEach(func() {
