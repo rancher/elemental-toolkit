a6484589766477514291fddf42b60e9263abee16
David Cassany <dcassany@suse.com>
2023-05-08 23:05:16 +0200

    add recovery test
    Signed-off-by: David Cassany <dcassany@suse.com>

--- tests/hooks/chroot_test.go
+++ tests/hooks/chroot_test.go
@@ -1,14 +1,18 @@
 package cos_test
 
 import (
+	"fmt"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
 	sut "github.com/rancher-sandbox/ele-testhelpers/vm"
+	comm "github.com/rancher/elemental-toolkit/tests/common"
 )
 
 // TODO this it just for first round of tests, at some point we should use
 // an image base on a build for a tagged main commit.
-const upgradeImg = "ghcr.io/davidcassany/elemental-green:v0.10.1-29-g9ab23ba5"
+//const upgradeImg = "ghcr.io/davidcassany/elemental-green:v0.10.1-29-g9ab23ba5"
 
 var _ = Describe("Elemental Feature tests", func() {
 	var s *sut.SUT
@@ -19,10 +23,11 @@ var _ = Describe("Elemental Feature tests", func() {
 
 	Context("After install", func() {
 		It("can run chroot hooks during upgrade and reset", func() {
+			By("setting /oem/chroot_hooks.yaml")
 			err := s.SendFile("../assets/chroot_hooks.yaml", "/oem/chroot_hooks.yaml", "0770")
 			Expect(err).ToNot(HaveOccurred())
-
-			out, err := s.Command(s.ElementalCmd("upgrade", "--system.uri", upgradeImg))
+			By(fmt.Sprintf("and upgrading the to %s", comm.UpgradeImage()))
+			out, err := s.Command(s.ElementalCmd("upgrade", "--system.uri", comm.UpgradeImage()))
 			Expect(err).ToNot(HaveOccurred())
 			Expect(out).Should(ContainSubstring("Upgrade completed"))
 			s.Reboot()
