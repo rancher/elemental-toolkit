f9d76605197f1afc2d1c873820a5b6db7303ad84
Fredrik Lönnegren <fredrik.lonnegren@suse.com>
2023-10-20 08:57:34 +0200

    prepare-test waits until system is expanded
    Signed-off-by: Fredrik Lönnegren <fredrik.lonnegren@suse.com>

---
+++ tests/wait-active/wait_test.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2022 - 2023 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package elemental_test
+
+import (
+	"time"
+
+	sut "github.com/rancher-sandbox/ele-testhelpers/vm"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Elemental booting fallback tests", func() {
+	var s *sut.SUT
+
+	BeforeEach(func() {
+		s = sut.NewSUT()
+		s.EventuallyConnects()
+	})
+	Context("Wait until system is expanded", func() {
+		It("eventually is active", func() {
+			Eventually(func() string {
+				out, _ := s.Command("cat /run/cos/active_mode")
+				return out
+			}, 1*time.Minute, 10*time.Second).Should(ContainSubstring("1"))
+		})
+	})
+})
