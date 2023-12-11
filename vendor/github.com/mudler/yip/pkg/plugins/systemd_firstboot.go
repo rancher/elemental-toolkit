2135927f965b2420b8bb8d4ae7d081fb5160372a
David Cassany <dcassany@suse.com>
2023-12-11 11:02:27 +0100

    Bump yip
    Signed-off-by: David Cassany <dcassany@suse.com>

--- vendor/github.com/mudler/yip/pkg/plugins/systemd_firstboot.go
+++ vendor/github.com/mudler/yip/pkg/plugins/systemd_firstboot.go
@@ -2,26 +2,34 @@ package plugins
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
-	"github.com/hashicorp/go-multierror"
 	"github.com/mudler/yip/pkg/logger"
 	"github.com/mudler/yip/pkg/schema"
 	"github.com/twpayne/go-vfs"
 )
 
 func SystemdFirstboot(l logger.Interface, s schema.Stage, fs vfs.FS, console Console) error {
-	var errs error
-
-	args := []string{}
+	var err error
+	var args []string
+	var out string
 
 	for k, v := range s.SystemdFirstBoot {
-		args = append(args, fmt.Sprintf("--%s=%s", strings.ToLower(k), v))
+		if v == "true" {
+			args = append(args, fmt.Sprintf("--%s", strings.ToLower(k)))
+		} else {
+			args = append(args, fmt.Sprintf("--%s=%s", strings.ToLower(k), v))
+		}
 	}
 
-	if err := console.RunTemplate(args, "systemd-firstboot %s"); err != nil {
-		errs = multierror.Append(errs, err)
+	if len(args) > 0 {
+		sort.Strings(args)
+		arguments := strings.Join(args, " ")
+		l.Debugf("running 'systemd-firstboot' with arguments: %s", arguments)
+		out, err = console.Run(fmt.Sprintf("systemd-firstboot %s", arguments))
+		l.Debugf("systemd-fristboot output: %s", out)
 	}
 
-	return errs
+	return err
 }
