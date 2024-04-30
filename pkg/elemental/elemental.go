5da65b61799267e7a5410c37d6042c9d1140a48b
David Cassany <dcassany@suse.com>
2024-04-30 12:34:15 +0200

    Fix error return
    Signed-off-by: David Cassany <dcassany@suse.com>

--- pkg/elemental/elemental.go
+++ pkg/elemental/elemental.go
@@ -617,7 +617,7 @@ func DumpSource(
 func MirrorRoot(c types.Config, target string, imgSrc *types.ImageSource) error {
 	err := DumpSource(c, target, imgSrc, utils.MirrorData)
 	if err != nil {
-		return nil
+		return err
 	}
 	return utils.CreateDirStructure(c.Fs, target)
 }
