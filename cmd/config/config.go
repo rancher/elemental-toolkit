ed23ec00477315d920d68819625e1be4a9e035c8
Gaurav Mehta <gaurav.mehta@suse.com>
2025-07-22 14:50:02 +1000

    Minor change to lookup devices using blkid
    Minor change to lookup devices using blkid and updating the upgradeSpec
    if needed. This may be needed when running elemental upgrade in
    multipathd systems
    
    Signed-off-by: Gaurav Mehta <gaurav.mehta@suse.com>

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -453,6 +453,11 @@ func ReadUpgradeSpec(r *types.RunConfig, flags *pflag.FlagSet, recoveryOnly bool
 	} else {
 		err = upgrade.Sanitize()
 	}
+	if err != nil {
+		return nil, fmt.Errorf("failed sanitizing upgrade spec: %v", err)
+	}
+
+	err = config.ReconcileUpgradeSpec(upgrade)
 	r.Logger.Debugf("Loaded upgrade UpgradeSpec: %s", litter.Sdump(upgrade))
 	return upgrade, err
 }
